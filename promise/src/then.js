d759c84a455c947034f376748331a83f3ec614c6
Jesse <jessey9527@gmail.com>
2018-03-23 01:02:30 +0800

    完成a+规范测试

--- promise/src/then.js
+++ promise/src/then.js
@@ -9,24 +9,33 @@ function getThen (promise, x) {
   }
 }
 
-function fulfill (promise, value) {
-  if (promise._status === CONFIG.PENDING) {
-    triggerTick(() => {
-      let i = 0
-      let quence = promise._sequence
-      while (quence[i]) {
-        try {
-          resolve(quence[i], (quence[i + CONFIG.FULFILLED] || promise.constructor.resolve)(value))
-        } catch (e) {
-          reject(quence[i], e)
-        }
+function nextTick (promise, result) {
+  triggerTick(() => {
+    let handle = promise._status === CONFIG.FULFILLED
+      ? promise.constructor.resolve
+      : promise.constructor.reject
 
-        i += 3
+    let i = 0
+    let quence = promise._sequence
+    while (quence[i]) {
+      try {
+        resolve(quence[i], (quence[i + promise._status] || handle)(result))
+      } catch (e) {
+        reject(quence[i], e)
       }
 
-      promise._sequence = []
-    })
+      i += 3
+    }
 
+    promise._sequence = []
+  })
+}
+
+function fulfill (promise, value) {
+  if (promise._status === CONFIG.PENDING) {
+    nextTick(promise, value)
+
+    promise._result = value
     promise._status = CONFIG.FULFILLED
   }
 }
@@ -35,16 +44,19 @@ export function resolve (promise, x) {
   if (promise === x) { // x 与 promise 相等
     reject(promise, new TypeError('x 不能与 promise 相等'))
   } else if (isObjectOrFunction(x)) {
-    let then = getThen(promise, x)
+    let then = getThen(promise, x) // 存储then，以保证 then 单次访问
     if (isFunction(then)) {
+      let alive = true
       try {
         then.call(x, function resolvePromise (y) {
-          resolve(promise, y)
+          if (alive) resolve(promise, y)
+          alive = false
         }, function rejectPromise (r) {
-          reject(promise, r)
+          if (alive) reject(promise, r)
+          alive = false
         })
       } catch (e) {
-        reject(promise, e)
+        if (alive) reject(promise, e) // then 执行过程出错,若未执行resolve/reject则以reject处理该promise
       }
     } else {
       fulfill(promise, x)
@@ -56,24 +68,15 @@ export function resolve (promise, x) {
 
 export function reject (promise, reason) {
   if (promise._status === CONFIG.PENDING) {
-    triggerTick(() => {
-      let i = 0
-      let quence = promise._sequence
-      while (quence[i]) {
-        try {
-          resolve(quence[i], (quence[i + CONFIG.REJECTED] || promise.constructor.reject)(reason))
-        } catch (e) {
-          reject(quence[i], e)
-        }
+    nextTick(promise, reason)
 
-        i += 3
-      }
-
-      promise._sequence = []
-    })
-
+    promise._result = reason
     promise._status = CONFIG.REJECTED
   }
+}
+
+function readyTick (promise) {
+  if (promise._status !== CONFIG.PENDING) nextTick(promise, promise._result)
 }
 
 // 这才是 then 开始
@@ -83,6 +86,7 @@ export function then (promise, onFulfilled, onRejected) {
   sequence[length] = new promise.constructor(noop)
   sequence[length + CONFIG.FULFILLED] = isFunction(onFulfilled) ? onFulfilled : null
   sequence[length + CONFIG.REJECTED] = isFunction(onRejected) ? onRejected : null
+  readyTick(promise) // 对于已解决的 promise 需要将结果，在下次 tick 中返回给then
 
   return sequence[length]
 }
